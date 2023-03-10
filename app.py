b717357a43c60c5a934b91a7f150102e7f611557
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-09 20:43:46 -0800

    12

--- app.py
+++ app.py
@@ -3,36 +3,54 @@ import numpy as np
 import plotly.graph_objs as go
 from plotly.subplots import make_subplots
 
-def cir(r0, K, theta, sigma, T=1.0, N=10, seed=None):
-    np.random.seed(seed)
-    dt = T/float(N)
-    rates = [r0]
-    for i in range(N):
-        dr = K*(theta-rates[-1])*dt + sigma*np.sqrt(rates[-1])*np.random.normal()
-        rates.append(rates[-1] + dr)
-    return range(N+1), rates
+# Define the CIR and Vasicek models for simulating interest rates
+def cir(r0, K, theta, sigma, T, N):
+    dt = float(T) / N
+    x = np.zeros(N + 1)
+    x[0] = r0
+    for i in range(1, N + 1):
+        dxt = K * (theta - x[i - 1]) * dt + sigma * np.sqrt(x[i - 1]) * np.random.normal()
+        x[i] = x[i - 1] + dxt
+    return np.arange(0, N + 1) * dt, x
 
-def vasicek(r0, K, theta, sigma, T=1.0, N=10, seed=None):
-    np.random.seed(seed)
-    dt = T/float(N)
-    rates = [r0]
-    for i in range(N):
-        dr = K*(theta-rates[-1])*dt + sigma*np.random.normal()
-        rates.append(rates[-1] + dr)
-    return range(N+1), rates
+def vasicek(r0, K, theta, sigma, T, N):
+    dt = float(T) / N
+    x = np.zeros(N + 1)
+    x[0] = r0
+    for i in range(1, N + 1):
+        dxt = K * (theta - x[i - 1]) * dt + sigma * np.random.normal()
+        x[i] = x[i - 1] + dxt
+    return np.arange(0, N + 1) * dt, x
 
-st.title("Interest Rate Simulation Dashboard")
+# Define the simulation parameters
+r0 = 0.1
+K = 0.5
+theta = 0.05
+sigma = 0.1
+T = 1
+N = 100
 
-r0 = st.slider("Initial Rate: r_0", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
-K = st.slider("Mean Reversion Rate: K", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
-theta = st.slider("Long-term Mean: theta", min_value=0.0, max_value=1.0, value=0.05, step=0.01)
-sigma = st.slider("Volatility: sigma", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
-T = st.slider("Time to Maturity (Years): T", min_value=0.1, max_value=10.0, value=1.0, step=0.1)
-N = st.slider("Number of Time Steps: N", min_value=1, max_value=100, value=10, step=1)
+# Simulate interest rates using the CIR and Vasicek models
+cir_x, cir_y = cir(r0, K, theta, sigma, T, N)
+vasicek_x, vasicek_y = vasicek(r0, K, theta, sigma, T, N)
 
-cir_x, cir_y = cir(r0=r0, K=K, theta=theta, sigma=sigma, T=T, N=N)
-vasicek_x, vasicek_y = vasicek(r0=r0, K=K, theta=theta, sigma=sigma, T=T, N=N)
-
-# Plot the results using Plotly
+# Create a new Plotly figure
 fig = make_subplots(rows=2, cols=1, subplot_titles=("CIR Model", "Vasicek Model"))
 
+# Add the CIR model data to the figure
+fig.add_trace(
+    go.Scatter(x=cir_x, y=cir_y, name="CIR Model"),
+    row=1, col=1
+)
+
+# Add the Vasicek model data to the figure
+fig.add_trace(
+    go.Scatter(x=vasicek_x, y=vasicek_y, name="Vasicek Model"),
+    row=2, col=1
+)
+
+# Update the layout of the figure
+fig.update_layout(height=600, width=800, title="Interest Rate Simulation")
+
+# Display the Plotly figure using Streamlit
+st.plotly_chart(fig)
