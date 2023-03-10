df3da5e88fa1d3d9e617f980a90f6754ac0e590d
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-09 21:36:58 -0800

    12

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=1.0, value=0.5, step
 K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
 theta = st.slider("theta: Long-term Mean", min_value=0.0, max_value=1.0, value=0.05, step=0.01)
 sigma = st.slider("sigma: Volatility", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
-T = st.slider("T: Time to Maturity (Years)", min_value=0.1, max_value=10.0, value=1.0, step=0.1)
+T = st.slider("T: Time to Maturity (Years)", min_value=0.1, max_value=1000.0, value=1.0, step=0.1)
 N = st.slider("N: Number of Time Steps", min_value=1, max_value=100, value=10, step=1)
 
 
