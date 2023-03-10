1ef7bbe39a93c8a5610080c84958e5d7bf9e644d
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-09 20:34:36 -0800

    12

--- app.py
+++ app.py
@@ -22,12 +22,12 @@ def vasicek(r0, K, theta, sigma, T=1.0, N=10, seed=None):
 
 st.title("Interest Rate Simulation Dashboard")
 
-r0 = st.slider("Initial Rate", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
-K = st.slider("Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
-theta = st.slider("Long-term Mean", min_value=0.0, max_value=1.0, value=0.05, step=0.01)
-sigma = st.slider("Volatility", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
-T = st.slider("Time to Maturity (Years)", min_value=0.1, max_value=10.0, value=1.0, step=0.1)
-N = st.slider("Number of Time Steps", min_value=1, max_value=100, value=10, step=1)
+r0 = st.slider("Initial Rate: r_0", min_value=0.0, max_value=1.0, value=0.5, step=0.01)
+K = st.slider("Mean Reversion Rate: K", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
+theta = st.slider("Long-term Mean: theta", min_value=0.0, max_value=1.0, value=0.05, step=0.01)
+sigma = st.slider("Volatility: sigma", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
+T = st.slider("Time to Maturity (Years): T", min_value=0.1, max_value=10.0, value=1.0, step=0.1)
+N = st.slider("Number of Time Steps: N", min_value=1, max_value=100, value=10, step=1)
 
 cir_x, cir_y = cir(r0=r0, K=K, theta=theta, sigma=sigma, T=T, N=N)
 vasicek_x, vasicek_y = vasicek(r0=r0, K=K, theta=theta, sigma=sigma, T=T, N=N)
