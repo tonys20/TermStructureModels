e10a3ddc966fcddad05cc5976905aba8f6a91107
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-11 14:46:35 -0800

    12

--- app.py
+++ app.py
@@ -46,9 +46,9 @@ def vasicek(r0, K, theta, sigma, T, N):
 
 # Define the simulation parameters
 
-r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=20.0, value=0.5, step=0.01)
-K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=20.0, value=0.1, step=0.01)
-theta = st.slider("theta: Long-term Mean", min_value=0.0, max_value=1.0, value=0.05, step=0.01)
+r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=10.0, value=0.5, step=0.01)
+K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
+theta = st.slider("theta: Long-term Mean", min_value=0.0, max_value=20.0, value=0.05, step=0.01)
 sigma = st.slider("sigma: Volatility", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
 T = st.slider("T: Time to Maturity (Years)", min_value=0.1, max_value=10.0, value=1.0, step=0.1)
 N = st.slider("N: Number of Time Steps", min_value=1, max_value=3650, value=10, step=1)
