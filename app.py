f278402b46d026f658b56edadefe7072811ce69b
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-11 14:26:00 -0800

    12

--- app.py
+++ app.py
@@ -50,7 +50,7 @@ K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.1,
 theta = st.slider("theta: Long-term Mean", min_value=0.0, max_value=1.0, value=0.05, step=0.01)
 sigma = st.slider("sigma: Volatility", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
 T = st.slider("T: Time to Maturity (Years)", min_value=0.1, max_value=10.0, value=1.0, step=0.1)
-N = st.slider("N: Number of Time Steps", min_value=1, max_value=365*T, value=10, step=1)
+N = st.slider("N: Number of Time Steps", min_value=1, max_value=3650, value=10, step=1)
 
 
 # Simulate interest rates using the CIR and Vasicek models
