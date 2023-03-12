ad6ac3ed72c971342b2a2f4ab7edbb394bb85dba
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-11 16:23:52 -0800

    restore

--- app.py
+++ app.py
@@ -46,16 +46,42 @@ def vasicek(r0, K, theta, sigma, T, N):
     return np.arange(0, N + 1) * dt, x
 
 # Define the simulation parameters
+r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=10.0, value=0.5, step=0.01)
+K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
+theta = st.slider("theta: Long-term Mean", min_value=0.0, max_value=20.0, value=0.05, step=0.01)
+sigma = st.slider("sigma: Volatility", min_value=0.0, max_value=5.0, value=0.1, step=0.01)
+T = st.slider("T: Time to Maturity (Years)", min_value=0.1, max_value=10.0, value=1.0, step=0.1)
+N = st.slider("N: Number of Time Steps", min_value=1, max_value=3650, value=10, step=1)
+# Simulate interest rates using the CIR and Vasicek models
+cir_x, cir_y = cir(r0, K, theta, sigma, T, N)
+vasicek_x, vasicek_y = vasicek(r0, K, theta, sigma, T, N)
+cir_neg_x, cir_neg_y = cir_neg(r0, K, theta, sigma, T, N)
 
 
+fig = make_subplots(rows=3, cols=1, subplot_titles=("CIR Model",'CIR ABS', "Vasicek Model"))
 
+# Add the CIR model data to the figure
+fig.add_trace(
+    go.Scatter(x=cir_x, y=cir_y, name="CIR Model"),
+    row=1, col=1
+)
 
+fig.add_trace(
+    go.Scatter(x=cir_neg_x, y=cir_neg_y, name = 'CIR abs hash'),
+    row=2, col=1
+)
+
+# Add the Vasicek model data to the figure
+fig.add_trace(
+    go.Scatter(x=vasicek_x, y=vasicek_y, name="Vasicek Model"),
+    row=3, col=1
+)
 
 # Update the layout of the figure
-
+fig.update_layout(height=600, width=800, title="Modern Term Structure Models")
 
 # Display the Plotly figure using Streamlit
-
+st.plotly_chart(fig)
 
 start_date = datetime.datetime(2017, 1, 1)
 end_date = datetime.datetime(2023, 3, 9)
@@ -77,7 +103,9 @@ for col in tbill_data.columns:
     hist_stats.loc[col,'vol'] = tbill_data[col].std()
 
 
-
+st.table(tbill_data.head())
+st.table(tbill_data.tail())
+st.table(hist_stats)
 
 # Define the CIR model function
 def cir_opt(r0, K, theta, sigma, T, N):
@@ -118,46 +146,6 @@ bounds = (0, 0.1)
 #result = opt.minimize_scalar(error_function, args=(r,), bounds=bounds, method = 'bounded')
 
 # Print the optimized parameters
-tabs = ['Sandbox', 'Calibration']
-active_tab = st.sidebar.radio('select tabs', tabs)
-
-
-if active_tab == 'Sandbox':
-    r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=10.0, value=0.5, step=0.01)
-    K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
-    theta = st.slider("theta: Long-term Mean", min_value=0.0, max_value=20.0, value=0.05, step=0.01)
-    sigma = st.slider("sigma: Volatility", min_value=0.0, max_value=5.0, value=0.1, step=0.01)
-    T = st.slider("T: Time to Maturity (Years)", min_value=0.1, max_value=10.0, value=1.0, step=0.1)
-    N = st.slider("N: Number of Time Steps", min_value=1, max_value=3650, value=10, step=1)
-    # Simulate interest rates using the CIR and Vasicek models
-    cir_x, cir_y = cir(r0, K, theta, sigma, T, N)
-    vasicek_x, vasicek_y = vasicek(r0, K, theta, sigma, T, N)
-    cir_neg_x, cir_neg_y = cir_neg(r0, K, theta, sigma, T, N)
-
-    fig = make_subplots(rows=3, cols=1, subplot_titles=("CIR Model",'CIR ABS', "Vasicek Model"))
-
-    # Add the CIR model data to the figure
-    fig.add_trace(
-        go.Scatter(x=cir_x, y=cir_y, name="CIR Model"),
-        row=1, col=1
-    )
-
-    fig.add_trace(
-        go.Scatter(x=cir_neg_x, y=cir_neg_y, name = 'CIR abs hash'),
-        row=2, col=1
-    )
-
-    # Add the Vasicek model data to the figure
-    fig.add_trace(
-        go.Scatter(x=vasicek_x, y=vasicek_y, name="Vasicek Model"),
-        row=3, col=1
-    )
-    fig.update_layout(height=600, width=800, title="Modern Term Structure Models")
-    st.plotly_chart(fig)
-else:
-    st.table(tbill_data.head())
-    st.table(tbill_data.tail())
-    st.table(hist_stats)
 
 st.write('Optimized Parameters:')
 #st.write(f'K = {result.x}')
