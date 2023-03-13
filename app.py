6a9c596c8faedc8be5a53e418b20bfb00026b6ab
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-13 00:16:28 -0700

    12

--- app.py
+++ app.py
@@ -80,21 +80,17 @@ with left_col:
     T_cal = 10.0
    
 
-    
-
-    # Define the simulation parameters
-
-    T = st.number_input("T: Time to Maturity (Years)", min_value=0, max_value=10, value=10 , step=1)
     N = st.number_input("N: Number of Time Steps", min_value=1, max_value=2000, value=12 , step=10)
     if st.button('Calibrate for r0, LT mean, volatility!'):
         r0_cal = tbill_data[rate_selected].iloc[-1]
         theta_cal = hist_stats.loc[rate_selected,'mean']
         sigma_cal = hist_stats.loc[rate_selected,'vol']*np.sqrt(N)
-    K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
+    # Define the simulation parameters
 
-
-    r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=6.0, value=float(r0_cal), step=0.01)
+    T = st.number_input("T: Time to Maturity (Years)", min_value=0, max_value=10, value=10 , step=1)
     
+    K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
+    r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=6.0, value=float(r0_cal), step=0.01)
     theta = st.slider("theta: Long-term Mean", min_value=0.0, max_value=6.0, value=float(theta_cal), step=0.01)
     sigma = st.slider("sigma: Volatility", min_value=0.0, max_value=5.0, value=float(sigma_cal), step=0.01)
     
