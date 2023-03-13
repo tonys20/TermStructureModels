1ba826500781dbef16873c5c20e45b0b723e520f
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-12 22:28:42 -0700

    12

--- app.py
+++ app.py
@@ -78,7 +78,7 @@ with left_col:
     T=10
 
     # Define the simulation parameters
-    r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=10.0, value=float(r0_cal), step=0.01)
+    r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=6.0, value=float(r0_cal), step=0.01)
     K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.1, step=0.01)
     theta = st.slider("theta: Long-term Mean", min_value=0.0, max_value=10.0, value=float(theta_cal), step=0.01)
     sigma = st.slider("sigma: Volatility", min_value=0.0, max_value=5.0, value=float(sigma_cal), step=0.01)
