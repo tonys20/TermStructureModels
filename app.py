b81be6dfc9c3850a6db98c4c4294934eb534babf
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-12 23:37:57 -0700

    12

--- app.py
+++ app.py
@@ -71,11 +71,11 @@ with left_col:
     '''
     options = ["DTB4WK","DTB3","DTB6","DTB1YR"]
     rate_selected = st.selectbox('Select the Treasury Bill rate to calibrate to', options)
-    r0_cal = 0
-    theta_cal = 0
-    sigma_cal = 0
-    N=12
-    T=10
+    r0_cal = 0.0
+    theta_cal = 0.0
+    sigma_cal = 0.0
+    N0=12
+    T0=10
 
     if st.button('Calibrate for r0, LT mean, volatility!'):
         r0_cal = tbill_data[rate_selected].iloc[-1]
@@ -86,11 +86,11 @@ with left_col:
 
     # Define the simulation parameters
     r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=6.0, value=float(r0_cal), step=0.01)
-    K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0, step=0.01)
+    K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=1.0, value=0.0, step=0.01)
     theta = st.slider("theta: Long-term Mean", min_value=0.0, max_value=6.0, value=float(theta_cal), step=0.01)
     sigma = st.slider("sigma: Volatility", min_value=0.0, max_value=5.0, value=float(sigma_cal), step=0.01)
-    T = st.slider("T: Time to Maturity (Years)", min_value=0.1, max_value=10.0, value=1.0, step=0.1)
-    N = st.number_input("N: Number of Time Steps", min_value=1, max_value=2000, value=10, step=10)
+    T = st.slider("T: Time to Maturity (Years)", min_value=0.1, max_value=10.0, value=T0, step=0.1)
+    N = st.number_input("N: Number of Time Steps", min_value=1, max_value=2000, value=N0, step=10)
 
 
 
