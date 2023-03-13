6a838beeba15a1b4a4c717abc8ddf28ea1a8da66
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-13 01:17:26 -0700

    1212

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ with left_col:
     
     K = st.slider("K: Mean Reversion Rate", min_value=0.0, max_value=3.0, value=0.0, step=0.01)
     r0 = st.slider("r0: Initial Rate", min_value=0.0, max_value=6.0, value=float(r0_cal), step=0.01)
-    theta = st.slider("theta: Long-term Mean", min_value=0.02, max_value=6.0, value=float(theta_cal), step=0.01)
+    theta = st.slider("theta: Long-term Mean", min_value=0.5, max_value=6.0, value=float(theta_cal), step=0.01)
     st.write(2*K*theta)
     sigma = st.slider("sigma: Volatility", min_value=0.0, max_value=np.sqrt(2*K*theta), value=float(sigma_cal), step=0.01)
     st.write(sigma**2)
