40ca27f30432b46b7687a5883b0a1821c1a8250e
tonys20 <81080785+tonys20@users.noreply.github.com>
2023-03-14 12:50:43 -0700

    12

--- app.py
+++ app.py
@@ -51,9 +51,9 @@ def get_tbill_data(item_ls, start_date, end_date):
     return tbill_data
 
 
-def monte_carlo(model, n_paths, r0, K, theta, sigma, start_date, end_date):
+def monte_carlo(model, n_paths, r0, K, theta, sigma, start_date, end_date,freq=1):
     T = (end_date-start_date).days/365
-    N = (end_date-start_date).days
+    N = (end_date-start_date).days*freq
     date_range = pd.date_range(start=start_date, end=end_date, periods=N+1)
     paths = np.zeros((n_paths, N+1))
     for i in range(n_paths):
@@ -228,6 +228,7 @@ with tab2:
     model = model_dic[st.selectbox('select a model for the simulation', model_names)]
     start_date = datetime.date.today()
     end_date = st.date_input('select end date', datetime.date.today())
-    date_range, paths = monte_carlo(model, n_paths, r0, K, theta, sigma, start_date, end_date)
+    freq = st.slider('Choose number of steps per day',  min_value=1, max_value=5, value=1, step=1)
+    date_range, paths = monte_carlo(model, n_paths, r0, K, theta, sigma, start_date, end_date, freq)
     fig = plot_sims(date_range, paths)
     st.plotly_chart(fig)
